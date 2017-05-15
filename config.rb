e97ca43a0082a520dded632e1ae27ff19bf7d86a
Stephane Mansour <stephane.mansour@gmail.com>
2017-05-15 17:38:43 +0100

    middleman done

--- config.rb
+++ config.rb
@@ -30,6 +30,9 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
 
 ###
 # Helpers
@@ -83,9 +86,11 @@ activate :deploy do |deploy|
   deploy.method = :git
   deploy.build_before = true
 
+
   # Optional Settings
   # deploy.remote = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
